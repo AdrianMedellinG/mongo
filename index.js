5ada097e22dd8d85d8dc246198bd7c4a9c83a2a9
AdrianMedellinG <adrian.medellin.garcia@hotmail.com>
2022-08-18 21:25:30 -0500

    Create Koder

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ require('dotenv').config()
 
 const mongoose = require('mongoose')
 const express = require('express')
+const { request } = require('express')
 const app = express()
+// Middlewares
+app.use(express.json()) // parseando a json
 
 const {
     DB_USERNAME,
@@ -12,6 +15,59 @@ const {
     DB_HOST,
     DB_NAME,
 } = process.env
+
+//Schemas de mongo
+const koderSchema = new mongoose.Schema({
+    nombre: {
+        type:String,
+        minlenght: 3,
+        maxlenght: 20,
+        required:true
+    },
+    modulo: {
+        type:String,
+    },
+    generacion: {
+        type:String,
+        required:true
+    },
+    edad: {
+        type:Number,
+        minlenght: 18,
+        maxlenght: 150,
+    },
+    sexo: {
+        type:String,
+        enum: ["F","M"]
+    }
+})
+
+//Modelos -> Collection
+const Koder = mongoose.model("koders", koderSchema)
+
+//Endpoints
+app.post("/koders", async (request,response)=>{
+    const { body } = request
+
+try {
+    const koder = await Koder.create(body)
+    console.log("koder",koder)
+        response.status(201)
+        response.json({
+            success: true,
+            data: {koder}
+        })
+} catch(error) {
+    console.log("error",error)
+        response.status(400)
+        response.json({
+            success: false,
+            message: error.message
+        })
+}  
+})
+
+
 
 const URL = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`
 //Conectando a base de datos
